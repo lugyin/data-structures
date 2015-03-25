94a7507090910593672b455ec63b836e34248800
lugyin <lugyin@gmail.com>
2015-03-25 16:36:10 -0700

    Data_structure_functional_shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,36 @@
 var Queue = function(){
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.count = 0;
+  //var storage = {};
+  _.extend(obj, queueMethods);
+
+  return obj;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value){
+  //this.storage[this.size] = value;
+  //storage[this.size] = value;
+  this[this.count] = value;
+  this.count++;
+};
+
+queueMethods.dequeue = function(){
+  if(this.count > 0){
+    var ans = this[0];
+    delete this[0];
+    for(var i = 0; i < this.count-1;i++){
+      var temp = this[i+1];
+      this[i] = temp;
+    }
+    this.count--;
+    return ans;
+  }
+};
+
+queueMethods.size = function(){
+  return this.count;
+};
 
 
--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,38 @@
 var Queue = function(){
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
 
-  // Implement the methods below
+  var storage = {};
+  // have var size
+  var size = 0;
 
   someInstance.enqueue = function(value){
+  // add value to obj at size
+  // have to increment all obj keys with +1
+    storage[size] = value;
+    size++;
   };
 
   someInstance.dequeue = function(){
+  // put first key/value into temp var
+    // delete first key/value
+    //return temp var
+    if(size > 0){
+    var dqObj = storage[0];
+    delete storage[0];
+    for(var i = 0; i < size-1; i++){
+      var temp = storage[i+1];
+      storage[i] = temp;
+    }
+    size--;
+    return dqObj;
+    }
+
   };
 
   someInstance.size = function(){
+  // return size
+  return size;
   };
 
   return someInstance;
