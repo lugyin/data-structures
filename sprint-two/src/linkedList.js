d399c18365b182e95c048cb1fdbb262e0dafec8d
lugyin <lugyin@gmail.com>
2015-03-26 11:33:23 -0700

    1st attempt

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,41 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(list.tail === null){
+      list.tail = newNode;
+    } else{
+      list.tail.next = newNode;
+      // list[list.tail].next = newNode;
+    }
+    list[newNode] = newNode;
+
+
+
   };
 
   list.removeHead = function(){
+    //remove first node
+      //change list.head to the new first node
+      // use the node.next of current head to point to new head.
+    if(list.head !== null){
+      var nextHead = list.head;
+      delete list[list.head]
+      list.head = nextHead.next;
+      return nextHead;
+    }
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(key.value === target)
+        return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
