1c2013bc4530deee49556db5e059d92b8ae0e360
lugyin <lugyin@gmail.com>
2015-03-27 14:56:06 -0700

    last problem

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,34 @@
 var BinarySearchTree = function(value){
+  var obj = {};
+  obj.storage = [];
+
+  obj.left = function(){
+
+  }
+
+  obj.right = {}
+
+  obj.insert = function(value){
+
+  }
+
+  obj.contains = function(value){
+
+  }
+
+  obj.depthFirstLog = function(cb){
+
+  }
 };
 
+var BinNode = function(){
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+}
+
+
+//A .left property, a binary search tree (BST) where all values are lower than than it the current value.
+// a right property, a BST where all values are higher than than it the current value.
+// a insert() method, which accepts a value and places in the tree in the correct position.
+// a .contains() method, which accepts a value and returns a boolean reflecting whether or not the value is contained in the tree.
+// A .depthFirstLog() method, which accepts a callback and executes it on every value contained in the tree.
+
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,58 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  //use i to find location in storage
+  var bucket = this._storage.get(i);
+  if(Array.isArray(bucket)){
+    // loop through bucket and match key
+    // if found update value
+    // if not create tuple and push into bucket
+    // does the original bucket in the storage array update also?
+    var isFound = false;
+    for(var j = 0; j<bucket.length;j++){
+      if(bucket[j][0]===k){
+        bucket[j][1] = v;
+        isFound = true;
+      }
+    }
+    if(!isFound){
+      bucket.push([k,v]);
+    }
+
+  }else{
+    //if not create array and use set insert the tuple of [k, v]
+    this._storage.set(i, [[k,v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  //use i to find location in storage
+    // loop through bucket each tuple to match idx 0
+    // if match return just v
+  var bucket = this._storage.get(i);
+  if(bucket){
+    for(var j = 0; j<bucket.length;j++){
+      if(bucket[j][0]===k){
+        return bucket[j][1];
+      }
+    }
+  }
+  return null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage.get(i);
+  if(bucket){
+    for(var j = 0; j<bucket.length;j++){
+      if(bucket[j][0]===k){
+        bucket.splice(j,1);
+
+      }
+    }
+  }
 
 };
 
